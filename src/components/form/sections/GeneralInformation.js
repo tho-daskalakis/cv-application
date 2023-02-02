b9fa14989e8bf92658e8d44281de7dd766c90cd8
Thomas <tho.daskalakis@gmail.com>
2023-02-02 17:28:57 +0200

    Update GeneralInformation to use controlled components

--- src/components/form/sections/GeneralInformation.js
+++ src/components/form/sections/GeneralInformation.js
@@ -1,25 +1,60 @@
 import React from 'react';
+import Input from '../../Input';
+import Label from '../../Label';
 
 class GeneralInformation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
     return (
       <fieldset className='column'>
         <legend>General Information</legend>
 
-        <label htmlFor='name'>Name:</label>
-        <input id='name' type='text' maxLength='40'></input>
+        <Label id='general-info-name' labelText='Name:' />
+        <Input
+          id='general-info-name'
+          type='text'
+          value={this.state.name}
+          onChange={this.handleNameChange}
+        />
 
-        <label htmlFor='title'>Title:</label>
-        <input id='title' type='text'></input>
+        <Label id='general-info-title' labelText='Title:' />
+        <Input
+          id='general-info-title'
+          type='text'
+          value={this.state.title}
+          onChange={this.handleTitleChange}
+        />
 
-        <label htmlFor='address'>Province / State, Country:</label>
-        <input id='address' type='text'></input>
+        <Label
+          id='general-info-address'
+          labelText='(Province / State), Country'
+        />
+        <Input
+          id='general-info-address'
+          type='text'
+          value={this.state.address}
+          onChange={this.handleAddressChange}
+        />
 
-        <label htmlFor='phone'>Phone:</label>
-        <input id='phone' type='text'></input>
+        <Label id='general-info-phone' labelText='Phone:' />
+        <Input
+          id='general-info-phone'
+          type='text'
+          value={this.state.phone}
+          onChange={this.handlePhoneChange}
+        />
 
-        <label htmlFor='email'>email:</label>
-        <input id='email' type='email'></input>
+        <Label id='general-info-email' labelText='e-mail:' />
+        <Input
+          id='general-info-email'
+          type='email'
+          value={this.state.email}
+          onChange={this.handleEmailChange}
+        />
       </fieldset>
     );
   }
